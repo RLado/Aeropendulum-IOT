c7df276260a73f10b0ac75885ceb7f0dc272aa37
Ricard Lado <ricardlador@iqs.edu>
2019-10-14 15:12:19 +0200

    Yet another quick fix

--- Docker_Setup/dockerSetup.sh
+++ Docker_Setup/dockerSetup.sh
@@ -2,4 +2,4 @@
 cd ..
 VDEVS=$(python3 Docker_Setup/dstp.py)
 
-docker build . -t aeropendulum && docker run --name Aeropendulum-IOT -p 5000:80 $VDEVS --restart=unless-stopped --security-opt seccomp="$(pwd)/docker_setup/seccomp_profile.json" smart4iv3/imrec_module &
+docker build . -t aeropendulum && docker run --name Aeropendulum-IOT -p 5000:80 $VDEVS --restart=unless-stopped --security-opt seccomp="$(pwd)/Docker_Setup/seccomp_profile.json" smart4iv3/imrec_module &
